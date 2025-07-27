0e9ccb9908b98ccf09e90fb9690adb5020d501f4
Sharukesh Kannan <133497349+sharu-kesh@users.noreply.github.com>
2025-07-27 14:32:43 +0530

    Time: 2581 ms (5.12%) | Memory: 25.8 MB (51.13%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.py
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -1,23 +1,29 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
         n, m = len(grid), len(grid[0])
-        def f(i, j1, j2):
-            if j1 < 0 or j1 >= m or j2 < 0 or j2 >= m:
-                return float('-inf')
-            if dp[i][j1][j2] != -1: return dp[i][j1][j2]
-            if i == n - 1:
+        dp = [[[0 for _ in range(m)] for _ in range(m)] for _ in range(n)]
+        for j1 in range(m):
+            for j2 in range(m):
                 if j1 == j2:
-                    return grid[i][j1]
-                return grid[i][j1] + grid[i][j2]
-            maxi = 0
-            for row in range(-1, 2, 1):
-                for col in range(-1, 2, 1):
-                    if j1 == j2:
-                        maxi = max(maxi, grid[i][j1] + f(i + 1, j1 + row,j2 +col))
-                    else:
-                        maxi = max(maxi, grid[i][j1] + grid[i][j2] + f(i + 1, row + j1, j2 + col))
-            dp[i][j1][j2] = maxi
-            return dp[i][j1][j2]
-        dp = [[[-1 for _ in range(m)] for _ in range(m)] for _ in range(n)]
-        return f(0, 0, m - 1)
+                    dp[-1][j1][j2] = grid[-1][j1]
+                else:
+                    dp[-1][j1][j2] = grid[-1][j1] + grid[-1][j2]
         
+        for i in range(n - 2, -1, -1):
+            for j1 in range(m):
+                for j2 in range(m):
+                    maxi = float('-inf')
+                    for dj1 in range(-1, 2, 1):
+                        for dj2 in range(-1, 2, 1):
+                            value = 0
+                            if j1 == j2:
+                                value = grid[i][j1]
+                            else:
+                                value = grid[i][j1] + grid[i][j2]
+                            if j1 + dj1 >= 0 and j1 + dj1 < m and j2 + dj2 >= 0 and j2 + dj2 < m:
+                                value += dp[i + 1][j1 + dj1][j2 + dj2]
+                            else:
+                                value += float('-inf')
+                            maxi = max(maxi, value)
+                    dp[i][j1][j2] = maxi
+        return dp[0][0][-1]
