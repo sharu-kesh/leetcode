a07b23c2fe7de40da4dd04f0899941180d78d6fd
Sharukesh Kannan <133497349+sharu-kesh@users.noreply.github.com>
2025-07-30 13:58:50 +0530

    Time: 167 ms (24.56%), Space: 18.6 MB (9.27%) - LeetHub

---
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -0,0 +1,13 @@
+class Solution:
+    def winnerOfGame(self, colors: str) -> bool:
+        A = B = 0
+        stack = []
+        for i in colors:
+            stack.append(i)
+            while len(stack) > 2 and stack[-2] ==  stack[-1] == stack[-3]:
+                removed = stack.pop()
+                if removed == 'A': A += 1
+                else: B += 1
+        if A <= B: return False
+        return True
+        
