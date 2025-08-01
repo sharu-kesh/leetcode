4849caf6c81799bf942d9775610ac32c791c8f10
Sharukesh Kannan <133497349+sharu-kesh@users.noreply.github.com>
2025-08-01 10:14:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (58.90%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,13 @@
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:
+        if n == 1:
+            return [[1]]
+        if n == 2:
+            return [[1], [1, 1]]
+        res = [[1], [1, 1]]
+        for i in range(2, n):
+            sol = [1] * (i + 1)
+            for j in range(1, i):
+                sol[j] = res[i - 1][j - 1] + res[i - 1][j]
+            res.append(sol)
+        return res        
