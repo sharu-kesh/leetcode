fccffc94c51df728b6af792b8bc62ef51d84de00
Sharukesh Kannan <133497349+sharu-kesh@users.noreply.github.com>
2025-08-14 23:53:03 +0530

    Time: 186 ms (42.89%) | Memory: 18.3 MB (57.31%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,16 +1,20 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         n, m = len(s), len(p)
-        mpp1 = Counter(p)
-        if n == m:
-            return [0] if Counter(s) == mpp1 else []
-        r = m - 1
-        l = 0
+        if n < m: return []
+        mpp = Counter(p)
+        mpp1 = Counter(s[:m])
         res = []
-        while r < n:
-            if Counter(s[l : r + 1]) == mpp1:
-                res.append(l)
-            l += 1
-            r += 1
+        # for i in range(n - m + 1):
+        #     temp = Counter(s[i : i + m])
+        #     if temp == mpp:
+        #         res.append(i)
+        # return res
+        if mpp == mpp1: res.append(0)
+
+        for i in range(m, n):
+            mpp1[s[i - m]] -= 1
+            mpp1[s[i]] += 1
+            if mpp == mpp1: res.append(i - m + 1)
         return res
         
