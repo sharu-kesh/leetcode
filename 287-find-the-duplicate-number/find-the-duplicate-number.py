1cdf59a987ea411706f99ec4c72c6e7a0b27947b
Sharukesh Kannan <133497349+sharu-kesh@users.noreply.github.com>
2025-08-29 12:57:04 +0530

    Time: 32 ms (39.09%) | Memory: 30 MB (50.93%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,8 +1,32 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        # use indices as markers
+
+        # for i in nums:
+        #     i = abs(i)
+        #     if nums[i - 1] < 0:
+        #         return i
+        #     nums[i - 1] = -1 * nums[i - 1]
+
+        # use bytearray to mark the elements that are visited
+
+        # seen = bytearray(len(nums) + 1)
+        # for i in nums:
+        #     if seen[i]:
+        #         return i
+        #     seen[i] = 1
+
+        slow = fast = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
         
-        for i in nums:
-            i = abs(i)
-            if nums[i - 1] < 0:
-                return i
-            nums[i - 1] = -1 * nums[i - 1]        
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        
+        return slow
