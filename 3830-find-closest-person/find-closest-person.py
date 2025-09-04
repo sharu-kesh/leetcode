5b349c85ad45372b09bb61fc600151e8c88c5e7a
Sharukesh Kannan <133497349+sharu-kesh@users.noreply.github.com>
2025-09-04 16:04:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (82.02%) - LeetSync

--- 3830-find-closest-person/find-closest-person.py
+++ 3830-find-closest-person/find-closest-person.py
@@ -1,17 +1,21 @@
 class Solution:
     def findClosest(self, x: int, y: int, z: int) -> int:
-        if x > z: step1 = -1
-        else: step1 = 1
-        if y > z: step2 = -1
-        else: step2 = 1
+        # if x > z: step1 = -1
+        # else: step1 = 1
+        # if y > z: step2 = -1
+        # else: step2 = 1
 
-        while True:
-            if x == z and y == z:
-                return 0
-            if x == z:
-                return 1
-            if y == z:
-                return 2
-            x += step1
-            y += step2
+        # while True:
+        #     if x == z and y == z:
+        #         return 0
+        #     if x == z:
+        #         return 1
+        #     if y == z:
+        #         return 2
+        #     x += step1
+        #     y += step2
+
+        if abs(x - z) < abs(y - z): return 1
+        elif abs(y - z) < abs(x - z): return 2
+        return 0
         
