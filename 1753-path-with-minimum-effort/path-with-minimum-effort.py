8d21d05621423df0007c917015abf952ab9998ae
Sharukesh Kannan <133497349+sharu-kesh@users.noreply.github.com>
2026-07-07 20:15:28 +0530

    Time: 239 ms (69.92%) | Memory: 21 MB (52.47%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -4,16 +4,15 @@ class Solution:
         minH = [(0, 0, 0)]
         dist = [[float('inf') for _ in range(m)] for _ in range(n)]
         dist[0][0] = 0
+        dir = [[-1, 0], [0, -1], [1, 0], [0, 1]]
         while minH:
             weight, i, j = heappop(minH)
-            dir = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+            if i == n - 1 and j == m - 1: return weight
             for dr, dc in dir:
                 r, c = i + dr, j + dc
                 if 0 <= r < n and 0 <= c < m:
                     new_weight = max(weight, abs(heights[i][j] - heights[r][c]))
                     if new_weight < dist[r][c]:
                         heappush(minH, (new_weight, r, c))
-                        dist[r][c] = new_weight
-        return dist[-1][-1]
-            
+                        dist[r][c] = new_weight            
         
