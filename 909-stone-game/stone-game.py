a7a0b4bc60f5c9f7aa3d6acc6111c00981ba4c02
Sharukesh Kannan <133497349+sharu-kesh@users.noreply.github.com>
2026-08-03 20:40:33 +0530

    Time: 71 ms (47.09%) | Memory: 19.3 MB (56.30%) - LeetSync

---
+++ 909-stone-game/stone-game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def stoneGame(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = list(nums)
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1])
+        return dp[-1] >= 0
